d4a0b8b8897edd5b4f6d783c63d32d286b09c3fc
manish <manish.7151.sharma@gmail.com>
2022-06-25 11:47:05 +0530

    Add project files.

---
+++ DiffrentWaysOfWritingQueries/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffrentWaysOfWritingQueries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lstint = new List<int> { 1, 2, 3, 4, 5, 7, 6, 8 };
+            var query_syntax = from obj in lstint
+                               where obj > 2
+                               select obj;
+
+            //foreach (var (item, index) in query_syntax.WithIndex())
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            query_syntax.ForEachWithIndex((item, idx) => Console.WriteLine("{0}: {1}", idx, item));
+
+            Console.ReadLine();
+        }
+
+
+        
+        //public static class EnumExtension
+        //{
+        //    public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self)
+        //       => self.Select((item, index) => (item, index));
+        //}
+    }
+
+
+    public static class ForEachExtensions
+    {
+        public static void ForEachWithIndex<T>(this IEnumerable<T> enumerable, Action<T, int> handler)
+        {
+            int idx = 0;
+            foreach (T item in enumerable)
+                handler(item, idx++);
+        }
+    }
+
+}
